1ee2c90c21807451b9f24478ceb0b1e545982cbe
francoChocobar <118401544+francoChocobar@users.noreply.github.com>
2023-05-02 13:12:36 -0300

    Add files via upload

---
+++ src/productsMock.js
@@ -0,0 +1,39 @@
+export const products = [
+  {
+    id: 1,
+    title: "Taza grande",
+    price: 1500,
+    stock: 2,
+    description: "Taza grande de ceramica",
+    category: "Tazas",
+    img: "https://res.cloudinary.com/dnqfh2chg/image/upload/v1669326932/free-metcon-4-zapatillas-de-entrenamiento-TQMCZg_fvia3m.png",
+  },
+  {
+    id: 2,
+    title: "Taza chica",
+    price: 1100,
+    stock: 15,
+    description: "Taza chica de ceramica",
+    category: "Tazas",
+    img: "https://res.cloudinary.com/dnqfh2chg/image/upload/v1669430010/foc364fsakkzmausrovn.jpg",
+  },
+  {
+    id: 3,
+    title: "Infusor",
+    price: 1000,
+    stock: 3,
+    description:
+      "Infusor de alumio para colocar dentro de la taza",
+    category: "Infusores",
+    img: "https://res.cloudinary.com/dnqfh2chg/image/upload/v1669430165/images_lujpmr.jpg",
+  },
+  {
+    id: 4,
+    title: "Frascos",
+    price: 1400,
+    stock: 5,
+    description: "Frascos de vidrio de 200 gr de hebras de te",
+    category: "Frascos",
+    img: "https://res.cloudinary.com/dnqfh2chg/image/upload/v1669430220/rtokwmt3j03bblbfhwhb.jpg",
+  },
+];
